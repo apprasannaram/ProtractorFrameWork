8872283e28e2d9d3a79c6b7ba999f02d87efb3d7
apprasannaram <prasanna218@gmail.com>
2019-11-14 16:32:02 +0530

    Calculator changes

--- Tests/Calculator.js
+++ Tests/Calculator.js
@@ -25,11 +25,11 @@ describe('Demo Calculator Tests', function () {
 
 
     });
-
+ ///prasanna addition
     it('Subtraction test', function () {
 
 
-        // browser.get('http://juliemr.github.io/protractor-demo/');
+        // hhhhbrowser.get('http://juliemr.github.io/protractor-demo/');
         Homepage.get('http://juliemr.github.io/protractor-demo/');
         //element(by.model('first')).sendKeys('3');      
         Homepage.enterFirstNumber('3');
@@ -38,16 +38,17 @@ describe('Demo Calculator Tests', function () {
         Homepage.enterSecondNumber('13');
 
         //element(by.css('[ng-click="doAddition()"]')).click();
-        Homepage.clickGo1();
+        Homepage.clickGo122();
 
         //let result=element(by.cssContainingText('.ng-binding','16'));
         //expect(result.getText()).toEqual('16');
         Homepage.VerifyResult('16');
         browser.sleep(3000);    
+        //presss
 
 
 
 
-    });
+    }); 
 
 });
